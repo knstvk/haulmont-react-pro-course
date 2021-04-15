eb101159b84ac14e70219a8bc379ffa87d5c6f4d
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-04-15 10:02:49 +0400

    Lesson 2 homework

---
+++ src/1-javascript/2-homeworks/lesson-2/4.js
@@ -0,0 +1,45 @@
+/**
+ * Задача 1.
+ *
+ * Вручную создать имплементацию функции `filter`.
+ * Логика работы ручной имплементации должна быть такой-же,
+ * как и у встроенного метода.
+ *
+ * Заметки:
+ * - Встроенный метод Array.prototype.filter использовать запрещено.
+ *
+ * Генерировать ошибки, если:
+ * - В качестве первого аргумента был передан не массив;
+ * - В качестве второго аргумента была передана не функция.
+ *
+ * Заметки:
+ * - Второй аргумент встроенного метода filter (thisArg) имплементировать не нужно.
+ */
+
+const array = ['Доброе утро!', 'Добрый вечер!', 3, 512, '#', 'До свидания!'];
+
+// Решение
+
+function filter(arr, predicate) {
+    if (!Array.isArray(arr)) {
+        throw 'Not an array'
+    }
+    if (typeof predicate !== 'function') {
+        throw 'Predicate is not a function'
+    }
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (predicate(arr[i], i, arr)) {
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+const filteredArray = filter(array, (element, index, arrayRef) => {
+    console.log(`${index}:`, element, arrayRef);
+
+    return element === 'Добрый вечер!';
+});
+
+console.log(filteredArray); // ['Добрый вечер!']
